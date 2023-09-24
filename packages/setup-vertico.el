e98c6e8a835a4961352555188a5b34d9f01312f2
Magnar Sveen <magnars@gmail.com>
2023-09-24 12:59:30 +0200

    IDO-like handling of directories when finding files with vertico

--- packages/setup-vertico.el
+++ packages/setup-vertico.el
@@ -8,4 +8,16 @@
   :init
   (vertico-mode))
 
+(use-package vertico-directory
+  :after vertico
+  :ensure nil ;; comes with vertico
+  ;; More convenient directory navigation commands
+  :bind (:map vertico-map
+              ("RET" . vertico-directory-enter)
+              ("DEL" . vertico-directory-delete-char)
+              ("M-DEL" . vertico-directory-delete-word))
+  ;; Tidy shadowed file names
+  :hook (rfn-eshadow-update-overlay . vertico-directory-tidy)
+  )
+
 (provide 'setup-vertico)
