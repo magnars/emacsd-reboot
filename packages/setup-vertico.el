c5c9eea95673217ca930935828ef20f3d8f8688f
Magnar Sveen <magnars@gmail.com>
2023-09-27 08:59:29 +0200

    Add orderless for more flexible vertico completion

--- packages/setup-vertico.el
+++ packages/setup-vertico.el
@@ -20,4 +20,15 @@
   :hook (rfn-eshadow-update-overlay . vertico-directory-tidy)
   )
 
+;; Use the `orderless' completion style.
+(use-package orderless
+  :ensure t
+  :init
+  ;; Configure a custom style dispatcher (see the Consult wiki)
+  ;; (setq orderless-style-dispatchers '(+orderless-consult-dispatch orderless-affix-dispatch)
+  ;;       orderless-component-separator #'orderless-escapable-split-on-space)
+  (setq completion-styles '(substring orderless basic)
+        completion-category-defaults nil
+        completion-category-overrides '((file (styles partial-completion)))))
+
 (provide 'setup-vertico)
