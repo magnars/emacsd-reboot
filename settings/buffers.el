1a7fab28461f76469a33b63a13439131a63ce459
Magnar Sveen <magnars@gmail.com>
2023-09-27 20:19:17 +0200

    Add missing create-scratch-buffer defun

--- settings/buffers.el
+++ settings/buffers.el
@@ -69,6 +69,20 @@
     (kill-new filename)
     (message "Copied file path %s" filename)))
 
+(defun create-scratch-buffer ()
+  "Create a new scratch buffer to work in. (could be *scratch* - *scratchX*)"
+  (interactive)
+  (let ((n 0)
+        bufname)
+    (while (progn
+             (setq bufname (concat "*scratch"
+                                   (if (= n 0) "" (int-to-string n))
+                                   "*"))
+             (setq n (1+ n))
+             (get-buffer bufname)))
+    (switch-to-buffer (get-buffer-create bufname))
+    (emacs-lisp-mode)))
+
 (defun touch-buffer-file ()
   (interactive)
   (insert " ")
