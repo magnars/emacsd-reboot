47db691deb32e905bfa0bea2afe188b0b53a5e66
Magnar Sveen <magnars@gmail.com>
2023-09-27 20:18:46 +0200

    Some highly miscellaneous keybindings

--- settings/extra-keybindings.el
+++ settings/extra-keybindings.el
@@ -3,4 +3,10 @@
 (global-set-key (kbd "C-x r q") 'save-buffers-kill-terminal)
 (global-set-key (kbd "C-x C-c") 'delete-frame)
 
+;; Search for more than just commands
+(global-set-key (kbd "<f1> a") 'apropos)
+
+;; Open url at point in a browser
+(global-set-key (kbd "C-x M-g") 'browse-url-at-point)
+
 (provide 'extra-keybindings)
