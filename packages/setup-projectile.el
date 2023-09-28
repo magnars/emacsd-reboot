ff86207b31da0df37bb53b50cef5103f6750a8d1
Magnar Sveen <magnars@gmail.com>
2023-09-28 08:21:44 +0200

    Change perspective when switching projects with projectile

--- packages/setup-projectile.el
+++ packages/setup-projectile.el
@@ -10,7 +10,16 @@
   (("s-p" . projectile-command-map))
 
   :config
-  (projectile-mode +1))
+  (projectile-mode +1)
 
+  (require 'setup-perspective)
+  (setq projectile-switch-project-action 'switch-perspective+find-file))
+
+(defun current-project-name ()
+  (cadr (reverse (split-string (projectile-project-root) "/"))))
+
+(defun switch-perspective+find-file ()
+  (with-perspective (current-project-name)
+    (projectile-find-file)))
 
 (provide 'setup-projectile)
