4bad11f587ef0e72beb1a989fb786606ed93885d
Magnar Sveen <magnars@gmail.com>
2023-09-28 08:19:26 +0200

    Remember turn on projectile-mode

--- packages/setup-projectile.el
+++ packages/setup-projectile.el
@@ -1,7 +1,16 @@
+;; Projectile
+;;
+;; A project interaction library. It provides a nice set of features operating
+;; on a project level without introducing external dependencies
+
 (use-package projectile
   :ensure t
   :diminish projectile-mode
   :bind-keymap
-  (("s-p" . projectile-command-map)))
+  (("s-p" . projectile-command-map))
+
+  :config
+  (projectile-mode +1))
+
 
 (provide 'setup-projectile)
