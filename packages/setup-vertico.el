22693c1364644439ffb05a335c64e0ffdedbbdc2
Magnar Sveen <magnars@gmail.com>
2023-09-29 08:03:42 +0200

    Minibuffer annotations with Marginalia

--- packages/setup-vertico.el
+++ packages/setup-vertico.el
@@ -39,4 +39,11 @@
   :init
   (savehist-mode))
 
+;; Enable rich annotations using the Marginalia package
+(use-package marginalia
+  :bind (:map minibuffer-local-map
+              ("M-A" . marginalia-cycle))
+
+  :init (marginalia-mode))
+
 (provide 'setup-vertico)
