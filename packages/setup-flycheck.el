65afed063a5134f46bc7805ce0f52a463fabdd54
Magnar Sveen <magnars@gmail.com>
2023-10-02 20:59:44 +0200

    Eagerly check for flycheck-fixes when there are errors, else wait.

--- packages/setup-flycheck.el
+++ packages/setup-flycheck.el
@@ -1,6 +1,16 @@
 (use-package flycheck
+  :diminish flycheck-mode
   :config (setq-default flycheck-disabled-checkers '(emacs-lisp-checkdoc emacs-lisp))
-  :init (global-flycheck-mode)
-  :diminish flycheck-mode)
+  :init
+  ;; Eagerly re-check whenever there are errors.
+  ;; When there are no errors, we're happy to wait for a save.
+  (add-hook 'flycheck-after-syntax-check-hook
+            (lambda ()
+              (setq-local flycheck-check-syntax-automatically
+                          (if flycheck-current-errors
+                              '(save idle-change mode-enabled)
+                            '(save mode-enabled)))))
+
+  (global-flycheck-mode))
 
 (provide 'setup-flycheck)
