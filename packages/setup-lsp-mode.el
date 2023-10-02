18016f753dd77470741e4da71ef88d4462b99e30
Magnar Sveen <magnars@gmail.com>
2023-10-02 13:55:52 +0200

    Hide lsp lenses

--- packages/setup-lsp-mode.el
+++ packages/setup-lsp-mode.el
@@ -4,7 +4,8 @@
          (clojurec-mode-hook . lsp))
 
   :init
-  (setq lsp-headerline-breadcrumb-enable nil)
+  (setq lsp-headerline-breadcrumb-enable nil) ;; Don't need file path in my buffer
+  (setq lsp-lens-enable nil) ;; Hide clutter (reference and test counts)
   (setq lsp-enable-indentation nil) ;; use clojure-mode indentation
   (setq lsp-eldoc-enable-hover nil) ;; use CIDER eldoc
   ;; (setq lsp-enable-completion-at-point nil) ;; consider CIDER vs LSP?
