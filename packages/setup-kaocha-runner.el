a1034d8826fb5fad24f3ac80c685a77cea9a42f3
Magnar Sveen <magnars@gmail.com>
2023-10-02 14:26:18 +0200

    Run relevant kaocha tests whenever loading a clojure file

--- packages/setup-kaocha-runner.el
+++ packages/setup-kaocha-runner.el
@@ -11,4 +11,26 @@
               ("C-c k w" . kaocha-runner-show-warnings)
               ("C-c k h" . kaocha-runner-hide-windows)))
 
+(defun kaocha-runner--is-test? ()
+  (string-match-p "/test/.+\.clj" (buffer-file-name)))
+
+(defun kaocha-runner-run-relevant-tests ()
+  (interactive)
+  (when (cljr--project-depends-on-p "kaocha")
+    (if (kaocha-runner--is-test?)
+        (kaocha-runner--run-tests
+         (kaocha-runner--testable-sym (cider-current-ns) nil nil)
+         nil t)
+      (let ((original-buffer (current-buffer)))
+        (save-window-excursion
+          (let* ((file (funcall significant-other-find-fn)))
+            (when (file-exists-p file)
+              (find-file file)))
+          (when (kaocha-runner--is-test?)
+            (kaocha-runner--run-tests
+             (kaocha-runner--testable-sym (cider-current-ns) nil nil)
+             nil t original-buffer)))))))
+
+(add-hook 'cider-file-loaded-hook #'kaocha-runner-run-relevant-tests)
+
 (provide 'setup-kaocha-runner)
