5529beee10f52aca817282c384121f53ae121b9b
Magnar Sveen <magnars@gmail.com>
2023-10-04 12:08:42 +0200

    Warn about missing nREPL instead of doing stupid things

--- packages/setup-cider.el
+++ packages/setup-cider.el
@@ -10,11 +10,39 @@
 (use-package cider
   :after (clojure-mode)
 
+  :config
+
+  ;; Warn about missing nREPL instead of doing stupid things
+  (my/shadow-cider-keys-with-warning)
+
   :custom
   ;; save files when evaluating them
   (cider-save-file-on-load t)
 
   ;; show stacktraces for everything, until https://github.com/clojure-emacs/cider/issues/3495 is solved
   (cider-clojure-compilation-error-phases nil))
+
+(defun nrepl-warn-when-not-connected ()
+  (interactive)
+  (message "Oops! You're not connected to an nREPL server. Please run M-x cider or M-x cider-jack-in to connect."))
+
+(defun my/shadow-cider-keys-with-warning ()
+  "Rebind all keys from `cider-mode-map` to `nrepl-warn-when-not-connected` in `clojure-mode-map`."
+  (interactive)
+  (map-keymap
+   (lambda (key def)
+     ;; Check if 'def' is a command or another keymap.
+     (cond ((commandp def)
+            ;; If 'def' is a command, rebind it in `clojure-mode-map`.
+            (define-key clojure-mode-map (vector key) 'nrepl-warn-when-not-connected))
+           ((keymapp def)
+            ;; If 'def' is another keymap, recursively apply the same process.
+            (map-keymap
+             (lambda (sub-key sub-def)
+               (when (commandp sub-def)
+                 (define-key clojure-mode-map (vconcat (vector key) (vector sub-key))
+                             'nrepl-warn-when-not-connected)))
+             def))))
+   cider-mode-map))
 
 (provide 'setup-cider)
