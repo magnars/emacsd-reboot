549c774e4bc3a0e3a3ebec3c4e6f6dbd845cb6ea
Magnar Sveen <magnars@gmail.com>
2023-10-09 14:11:53 +0200

    Add whitespace-cleanup-mode to clean buffers when saved

---
+++ packages/setup-whitespace-cleanup-mode.el
@@ -0,0 +1,10 @@
+;; whitespace-cleanup-mode.el
+;;
+;; A minor mode that will intelligently call whitespace-cleanup before buffers
+;; are saved.
+
+(use-package whitespace-cleanup-mode
+  :init
+  (global-whitespace-cleanup-mode))
+
+(provide 'setup-whitespace-cleanup-mode)
