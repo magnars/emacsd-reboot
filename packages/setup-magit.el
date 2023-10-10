8fec74834fb98b43b1c217f56f25956e7c74deaf
Magnar Sveen <magnars@gmail.com>
2023-10-10 14:36:48 +0200

    Toggle pair-programming `C-c p` to add Co-author to git commits.

--- packages/setup-magit.el
+++ packages/setup-magit.el
@@ -13,6 +13,7 @@
                       unstage-all-changes))
 
   :bind (("C-x m" . magit-status)
+         ("C-c p" . magit-toggle-pair-programming-mode)
          (:map magit-status-mode-map
                ("q" . magit-quit)))
 
@@ -33,5 +34,34 @@ configuration stored by magit-status-fullscreen"
   (kill-magit-buffers)
   (funcall magit-bury-buffer-function 'kill-buffer)
   (jump-to-register :magit-fullscreen))
+
+(defvar magit-pair-programming-partner nil)
+
+(defun is-commit-message-buffer? ()
+  (equal (buffer-file-name-body) "COMMIT_EDITMSG"))
+
+(defun magit-insert-pair-programming-co-author ()
+  (apply #'git-commit-insert-header "Co-authored-by" magit-pair-programming-partner))
+
+(defun magit-enable-pair-programming-mode (details)
+  (setq magit-pair-programming-partner details)
+  (when (is-commit-message-buffer?)
+    (magit-insert-pair-programming-co-author))
+  (add-hook 'git-commit-setup-hook 'magit-insert-pair-programming-co-author))
+
+(defun magit-disable-pair-programming-mode ()
+  (setq magit-pair-programming-partner nil)
+  (when (is-commit-message-buffer?)
+    (save-excursion
+      (goto-char (point-min))
+      (flush-lines "Co-authored-by")))
+  (remove-hook 'git-commit-setup-hook 'magit-insert-pair-programming-co-author))
+
+(defun magit-toggle-pair-programming-mode ()
+  (interactive)
+  (if magit-pair-programming-partner
+      (magit-disable-pair-programming-mode)
+    (magit-enable-pair-programming-mode
+     (git-commit-read-ident "Pair programming with"))))
 
 (provide 'setup-magit)
