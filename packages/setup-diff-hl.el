82729c862b80dae62751daeac5c45b82aa1d0081
Magnar Sveen <magnars@gmail.com>
2023-10-11 08:20:05 +0200

    Highlight uncommitted changes in the gutter

---
+++ packages/setup-diff-hl.el
@@ -0,0 +1,13 @@
+;; diff-hl-mode
+;;
+;; Highlights uncommitted changes on the left side of the window (area also
+;; known as the "gutter"), allows you to jump between and revert them
+;; selectively.
+
+(use-package diff-hl
+  :config
+  (global-diff-hl-mode)
+  (add-hook 'magit-pre-refresh-hook 'diff-hl-magit-pre-refresh)
+  (add-hook 'magit-post-refresh-hook 'diff-hl-magit-post-refresh))
+
+(provide 'setup-diff-hl)
