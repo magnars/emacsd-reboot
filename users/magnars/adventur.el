78dc282c41372a30cdf241aab8d50653fc53ab57
Magnar Sveen <magnars@gmail.com>
2023-10-12 19:07:16 +0200

    users/magnars: Set up adventur-mode.el

--- users/magnars/adventur.el
+++ users/magnars/adventur.el
@@ -1,3 +1,10 @@
-;; TODO: Set up Adventur Delux editing mode
+;; Set up Adventur Delux editing mode
 
-(setq my/test 123)
+(straight-use-package
+ '(adventur-mode :type git :host github :protocol ssh :repo "magnars/adventur-mode"))
+
+(autoload 'adventur-mode "adventur-mode")
+(add-to-list 'auto-mode-alist '("\\.adv$" . adventur-mode))
+
+(global-set-key (kbd "C-x p n") (λ (projectile-switch-project-by-name "~/projects/no-adventur")))
+(global-set-key (kbd "C-x p m") (λ (with-perspective "eventyr" (find-file "~/projects/eventyr/master/notat.org"))))
