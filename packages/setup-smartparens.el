1fdd9a283b5bad519f97784d816f3eeec53f8b0f
Magnar Sveen <magnars@gmail.com>
2023-10-15 18:52:10 +0200

    Add smartparens for those non-lisps

---
+++ packages/setup-smartparens.el
@@ -0,0 +1,15 @@
+;; Smartparens
+;;
+;; Smartparens is a minor mode for dealing with pairs in Emacs. Like a
+;; paredit-lite for non-lisps.
+
+(use-package smartparens
+  :init
+  (require 'smartparens-config)
+  (setq sp-autoescape-string-quote nil)
+  (--each '(css-mode-hook
+            js-mode-hook
+            markdown-mode)
+    (add-hook it 'turn-on-smartparens-mode)))
+
+(provide 'setup-smartparens)
