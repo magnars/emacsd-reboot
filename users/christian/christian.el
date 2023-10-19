35b442f5fc828cca7f9552c4698be28da6effa87
Christian Johansen <christian@cjohansen.no>
2023-10-19 09:59:14 +0200

    Add user-specific settings for Christian

---
+++ users/christian/christian.el
@@ -0,0 +1,12 @@
+;; Custom own settings
+
+(require 'mattilsynet)
+
+;; Configure pair-programming-mode
+;;
+;; Suggest these first:
+(setq my/pair-programming-usual-suspects '("Magnar Sveen <magnar.sveen@mattilsynet.no>"
+                                           "Magnar Sveen <magnars@gmail.com>"))
+;;
+;; Don't suggest these:
+(setq my/pair-programming-myself '("Christian Johansen"))
