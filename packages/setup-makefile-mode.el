d63d5dc6ef52b7ab9db40a8d2866b6b450bd4eb1
Magnar Sveen <magnars@gmail.com>
2023-11-22 19:45:21 +0100

    Initial stab at s-m to run targets from Makefile

--- packages/setup-makefile-mode.el
+++ packages/setup-makefile-mode.el
@@ -52,4 +52,39 @@
           (lambda ()
             (setq-local indent-line-function 'my/makefile-indent-line)))
 
+(defun makefile-find-targets ()
+  "Find all targets in a Makefile."
+  (let (targets)
+    (save-excursion
+      (goto-char (point-min))
+      (while (re-search-forward "^\\([^:#\t\n]+?\\):" nil t)
+        (let ((target (match-string-no-properties 1)))
+          (unless (s-equals? target ".PHONY")
+            (setq targets (cons (string-trim target) targets))))))
+    (reverse targets)))
+
+(defun shorten-path (path)
+  "Shortens the file PATH by replacing the home directory with ~."
+  (let ((home (expand-file-name "~")))
+    (if (string-prefix-p home path)
+        (concat "~" (substring path (length home)))
+      path)))
+
+(defun makefile-invoke-target ()
+  (interactive)
+  (let ((file (concat (projectile-project-root) "Makefile"))
+        (short-dir (shorten-path (projectile-project-root)))
+        (default-directory (projectile-project-root)))
+    (if (file-exists-p file)
+        (async-shell-command
+         (completing-read (format "Make in %s: " short-dir)
+                          (--map
+                           (concat "make " it)
+                           (with-temp-buffer
+                             (insert-file-contents file)
+                             (makefile-find-targets)))))
+      (message "No Makefile found in %s" short-dir))))
+
+(global-set-key (kbd "s-m") 'makefile-invoke-target)
+
 (provide 'setup-makefile-mode)
