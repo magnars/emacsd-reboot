770e5381084eb08e71750c91796c702a8faffca7
Magnar Sveen <magnars@gmail.com>
2024-01-15 12:03:41 +0100

    Set up restclient mode

---
+++ packages/setup-restclient.el
@@ -0,0 +1,7 @@
+(use-package restclient
+  :defer t)
+
+(add-to-list 'auto-mode-alist '("\\.restclient$" . restclient-mode))
+(add-hook 'restclient-mode-hook 'turn-on-smartparens-mode)
+
+(provide 'setup-restclient)
