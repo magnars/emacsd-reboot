a0e8836ae3fde4c8d7ec760e2fae6ef43d9c4c52
Magnar Sveen <magnars@gmail.com>
2024-01-29 09:05:16 +0100

    Ignore unknown-at rules for CSS, due to Tailwind
    It would have been decidedly better to use css.experimental.customData to add
    the tailwind specific at-directives, but this ^^^^^^^^^^^^^^ word seems to be
    there for a reason. I can't get it to work.

--- packages/setup-css-mode.el
+++ packages/setup-css-mode.el
@@ -4,7 +4,9 @@
 
 (use-package lsp-tailwindcss
   :init
-  (setq lsp-tailwindcss-add-on-mode t))
+  (setq lsp-tailwindcss-add-on-mode t)
+  (setq lsp-css-lint-unknown-at-rules "ignore") ;; ignore @tailwind and @apply (and all other such errors, oops)
+  )
 
 (require 'lsp-tailwindcss)
 
