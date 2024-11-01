bbe321df2b57bcc0d0d27a864fdc53c6aa7f9648
Teodor Heggelund <git@teod.eu>
2024-11-01 15:52:05 +0100

    Configure Markdown and expand-region for Teodor

--- users/teodorlu/teodorlu.el
+++ users/teodorlu/teodorlu.el
@@ -1,5 +1,13 @@
 (require 'mattilsynet)
 
+;; Don't auto-wrap lines, I like one line per sentence
+;; https://sive.rs/1s
+(remove-hook 'markdown-mode-hook 'auto-fill-mode)
+
+;; I can't get C-@ to work, use C-ø
+(global-set-key (kbd "C-ø") 'er/expand-region)
+(global-set-key (kbd "C-Ø") 'er/contract-region)
+
 (defun teodorlu-magnar-emacs-cheat-sheet ()
   (interactive)
   (find-file (expand-file-name "~/.emacs.d/README.md")))
