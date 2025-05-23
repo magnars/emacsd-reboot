547dce7c379d82fdf67306ffe73f60cdecc6ce03
Sigmund Hansen <sigmund.hansen@mattilsynet.no>
2025-05-23 22:17:49 +0200

    Add my, Sigmund's, personal settings

---
+++ users/sigmund/sigmund.el
@@ -0,0 +1,24 @@
+;; Custom own settings
+
+;;Configure pair-programming-mode
+(setq my/pair-programming-myself '("Sigmund Hansen"))
+
+;; PlantUML
+(use-package plantuml-mode)
+(use-package flycheck-plantuml)
+
+(setq plantuml-jar-path
+      (concat (expand-file-name "~/bin/")
+              "plantuml.jar"))
+(setq plantuml-default-exec-mode 'jar)
+(add-to-list 'auto-mode-alist '("\\.plantuml\\'" . plantuml-mode))
+
+(with-eval-after-load 'org
+  (add-to-list 'org-src-lang-modes '("plantuml" . plantuml)))
+
+(with-eval-after-load 'flycheck
+  (require 'flycheck-plantuml)
+  (flycheck-plantuml-setup))
+
+;; Side by side ediff
+(setq ediff-split-window-function 'split-window-vertically)
