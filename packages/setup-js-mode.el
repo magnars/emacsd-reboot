baeaa2e5c99593f1385cf18672ed803591cde4a6
Sigmund Hansen <sigmund.hansen@mattilsynet.no>
2025-06-18 19:11:39 +0200

    Add CJS and MJS file extensions to the JS mode list

--- packages/setup-js-mode.el
+++ packages/setup-js-mode.el
@@ -5,4 +5,8 @@
   :custom
   (js-indent-level 2))
 
+;; Add CommonJS and ES6 Module JS extensions to the js-mode list
+(add-to-list 'auto-mode-alist '("\\.cjs$" . js-mode))
+(add-to-list 'auto-mode-alist '("\\.mjs$" . js-mode))
+
 (provide 'setup-js-mode)
