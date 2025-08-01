dcae26354809808b605cac8f55007ddb9bdff23a
Sigmund Hansen <github@chickensoft.com>
2025-08-01 22:43:22 +0200

    Bind expand region to same keys on normal Norwegian keyboards
    C-@ works great on Norwegian Mac keyboards, but the @ is usually located on 2 on
    standard keyboards. The @ on Mac keyboards occupy the ' key. This makes the
    config work better across operating systems, as you don't have to change your
    keyboard shortcut muscle memory.

--- packages/setup-expand-region.el
+++ packages/setup-expand-region.el
@@ -1,6 +1,7 @@
 (use-package expand-region
   :straight t
   :defer t
-  :bind ("C-@" . er/expand-region))
+  :bind (("C-@" . er/expand-region)
+         ("C-'" . er/expand-region)))
 
 (provide 'setup-expand-region)
