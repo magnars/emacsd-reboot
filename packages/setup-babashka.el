e863bb13aab4d284d1648bd6b266c1b6f77109e9
Teodor Heggelund <git@teod.eu>
2025-10-21 14:47:37 +0200

    Introduser interaktiv funksjon for å kjøre Babashka tasks
    For nå, kjør med
    
        M-x babashka-run-task

---
+++ packages/setup-babashka.el
@@ -0,0 +1,11 @@
+(defun babashka-tasks ()
+  (s-lines (s-trim (shell-command-to-string (concat "bb -e " (shell-quote-argument "(run! println (keys (:tasks (read-string (slurp \"bb.edn\")))))"))))))
+
+(defun babashka-run-task ()
+  (interactive)
+  (let* ((default-directory (projectile-project-root))
+         (task (completing-read "Select task > " (babashka-tasks))))
+    (async-shell-command (concat "bb " (shell-quote-argument task)))
+    (shell-command (concat "bb " (shell-quote-argument task)))))
+
+(provide 'setup-babashka)
