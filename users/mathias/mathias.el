bc855e2933024f6fdd26e51c4b9c630d039aa820
Mathias Iversen <mathias.iversen@mattilsynet.no>
2025-12-20 21:26:04 +0100

    Actually commit helpful after months in the working tree

--- users/mathias/mathias.el
+++ users/mathias/mathias.el
@@ -17,3 +17,12 @@
 
 ;; Always split to the right
 (setq split-height-threshold nil)
+
+(use-package helpful)
+
+(global-set-key (kbd "<f1> f") #'helpful-callable)
+(global-set-key (kbd "<f1> v") #'helpful-variable)
+(global-set-key (kbd "<f1> k") #'helpful-key)
+(global-set-key (kbd "<f1> x") #'helpful-command)
+
+(provide 'setup-helpful)
