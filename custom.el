452aa7aec50993e70bf5a09cfd9daef3d08653eb
Sigmund Hansen <sigmund.hansen@mattilsynet.no>
2026-02-10 21:07:35 +0100

    Fix default black theme hash in after Lexical binding cookiefication

--- custom.el
+++ custom.el
@@ -1,10 +1,11 @@
+;; -*- lexical-binding: t; -*-
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("f0c81c4aab004ac69123f871e45f1a081264e8910ad23ab7a673ca8f0818a838" "05692bda554c178fafe15cc3e6ab09539e7db4846eb9bb6272b97068c055a903" default))
+   '("75cb4262913dc328a0c56355094866c4c8f2ffc274931e953a61a6129956fbd4" "f0c81c4aab004ac69123f871e45f1a081264e8910ad23ab7a673ca8f0818a838" default))
  '(package-selected-packages
    '(diff-hl exec-path-from-shell whitespace-cleanup-mode clj-refactor kaocha-runner marginalia which-key cider diminish move-text undo-fu-session vundo undo-fu consult-flycheck orderless consult flycheck datomic-snippets yasnippet projectile wgrep-deadgrep wgrep deadgrep vertico-directory vertico lsp-mode clojure-mode s multiple-cursors expand-region paredit magit))
  '(safe-local-variable-values
