bca94208875c1aca28198fcc59bb6b58c5013fd0
Mathias Iversen <mathias.iversen@mattilsynet.no>
2026-03-12 12:23:58 +0100

    Show the first line of the folded section

--- packages/setup-fold-this.el
+++ packages/setup-fold-this.el
@@ -5,9 +5,11 @@
     (let ((delimiters '(?\( ?\[ ?{ ?\")))
       (unless (memq (char-after) delimiters)
         (ignore-errors (backward-up-list 1 t nil)))
-      (set-mark (point))
-      (forward-sexp)
-      (fold-this (mark) (point)))))
+      (let ((beg (point))
+            (line-end (line-end-position)))
+        (forward-sexp)
+        (fold-this beg (point)
+                   (buffer-substring-no-properties beg line-end))))))
 
 (use-package fold-this
   :bind (("C-<tab>" . my/fold-this-datastructure))
