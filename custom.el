d0b2ab6ed35942260c7221e2a8c91d65b13cef71
Mathias Iversen <mathias.iversen@mattilsynet.no>
2026-05-04 15:20:26 +0200

    Add Matnyttig's magit-subject-commit-range as a safe local variable

--- custom.el
+++ custom.el
@@ -5,11 +5,18 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("75cb4262913dc328a0c56355094866c4c8f2ffc274931e953a61a6129956fbd4" "f0c81c4aab004ac69123f871e45f1a081264e8910ad23ab7a673ca8f0818a838" default))
+   '("75cb4262913dc328a0c56355094866c4c8f2ffc274931e953a61a6129956fbd4"
+     "f0c81c4aab004ac69123f871e45f1a081264e8910ad23ab7a673ca8f0818a838" default))
  '(package-selected-packages
-   '(diff-hl exec-path-from-shell whitespace-cleanup-mode clj-refactor kaocha-runner marginalia which-key cider diminish move-text undo-fu-session vundo undo-fu consult-flycheck orderless consult flycheck datomic-snippets yasnippet projectile wgrep-deadgrep wgrep deadgrep vertico-directory vertico lsp-mode clojure-mode s multiple-cursors expand-region paredit magit))
+   '(diff-hl exec-path-from-shell whitespace-cleanup-mode clj-refactor
+             kaocha-runner marginalia which-key cider diminish move-text
+             undo-fu-session vundo undo-fu consult-flycheck orderless consult
+             flycheck datomic-snippets yasnippet projectile wgrep-deadgrep wgrep
+             deadgrep vertico-directory vertico lsp-mode clojure-mode s
+             multiple-cursors expand-region paredit magit))
  '(safe-local-variable-values
-   '((cider-clojure-cli-aliases . "-A:dev")
+   '((magit-subject-commit-range . "d7425eae^..HEAD")
+     (cider-clojure-cli-aliases . "-A:dev")
      (cider-figwheel-main-default-options . ":ui")
      (cider-preferred-build-tool . clojure-cli)
      (cider-default-cljs-repl . figwheel-main)
