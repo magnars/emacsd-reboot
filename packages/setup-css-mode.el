db2271bb69b042247223b9df3c8c51589be34640
Mathias Iversen <mathias.iversen@mattilsynet.no>
2026-06-08 17:19:55 +0200

    This package only exists on GitHub
    When reinstalling Emacs v29 I needed to specify the package to use straight
    before Emacs would install the package. `lsp-tailwindcss` does not exist on elpa
    or melpa, only GitHub.

--- packages/setup-css-mode.el
+++ packages/setup-css-mode.el
@@ -4,6 +4,7 @@
 (setq css-fontify-colors nil)
 
 (use-package lsp-tailwindcss
+  :straight t
   :init
   (setq lsp-tailwindcss-add-on-mode t)
   (setq lsp-css-lint-unknown-at-rules "ignore") ;; ignore @tailwind and @apply (and all other such errors, oops)
