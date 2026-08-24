9c1ee74e4b853182c1a07c711576b98fbac55071
Teodor Heggelund <git@teod.eu>
2026-08-24 14:00:56 +0200

    Teodor: include line and column number in scene-yank-path

--- users/teodorlu/scene.el
+++ users/teodorlu/scene.el
@@ -20,14 +20,16 @@ The scene directory is the parent directory of Projectile's project root."
 
 ;;;###autoload
 (defun scene-yank-path ()
-  "Yank the current file path relative to the scene directory."
+  "Yank the current file path and line relative to the scene directory."
   (interactive)
   (unless buffer-file-name
     (user-error "Current buffer is not visiting a file"))
   (let ((scene-dir (scene-directory (file-name-directory buffer-file-name))))
     (unless scene-dir
       (user-error "Could not find scene directory for %s" buffer-file-name))
-    (kill-new (file-relative-name buffer-file-name scene-dir))))
+    (kill-new (format "%s:%d"
+                      (file-relative-name buffer-file-name scene-dir)
+                      (line-number-at-pos)))))
 
 (provide 'scene)
 
