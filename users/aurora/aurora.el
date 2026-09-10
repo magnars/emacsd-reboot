f0260db7855445f6e87564395c93c72f31e42da9
Aurora Liang Klæboe <aurora.liang.klaboe@mattilsynet.no>
2026-09-10 11:38:20 +0200

    Aurora: Add convenience methods in org-mode for studies

--- users/aurora/aurora.el
+++ users/aurora/aurora.el
@@ -27,3 +27,50 @@
 
 ;; Font
 (add-to-list 'default-frame-alist '(font . "Source Code Pro"))
+
+
+;;;; Org-Mode for taking notes
+(use-package org
+  :defer t
+  :hook (org-mode . (lambda ()
+                      (setq fill-column 80) ;; break lines at 80 chrs
+                      (auto-fill-mode 1)))
+  :config
+  (setq org-pretty-entities t
+        org-hide-leading-stars t
+        org-hide-emphasis-markers t)
+  :bind (:map org-mode-map
+              ("C-c C-k" . 'save-buffer)
+              ("M-<return>" . org-meta-return)
+              ("C-c e" . org-latex-export-to-pdf)
+              ("C-c s" . (lambda ()
+                           (interactive)
+                           (org-insert-structure-template "src")
+                           (forward-line -1)
+                           (end-of-line)))
+              ("C-c v" . (lambda ()
+                           (interactive)
+                           (org-insert-structure-template "verse")
+                           (insert "\n")
+                           (forward-line -1)))
+              ("C-c x" . (lambda ()
+                           (interactive)
+                           (org-insert-structure-template "example")
+                           (forward-line -1)
+                           (end-of-line)
+                           (insert " -i\n")))))
+
+;;;; LaTeX
+(use-package auctex
+  :defer t)
+
+;; LaTeX Back-End for Org Export Engine
+(use-package ox-latex
+  :ensure nil
+  :after org
+  :config
+  (setenv "PATH" (concat "/Library/TeX/texbin:" (getenv "PATH")))
+  (add-to-list 'exec-path "/Library/TeX/texbin")
+  (setq org-latex-pdf-process
+        '("latexmk -xelatex -synctex=1 -shell-escape -interaction=nonstopmode -f %f"))
+  (setq org-latex-listings 'minted))
